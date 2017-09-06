3a2b9183b044d9e78ef9e50e27710fa801e517e8
jack-forrester <JackForrester@Jacks-MacBook-Air.local>
2017-09-06 13:34:24 +0100

    using LiveScoreAPI

--- ExtractingInfo.py
+++ ExtractingInfo.py
@@ -5,6 +5,13 @@ URL = LiveScoresAPI.FindURL()
 scoreInfo = LiveScoresAPI.getScoreInfo(URL)
 rows = LiveScoresAPI.getRows(scoreInfo)
 
-    #Import titles
+#Import titles
 teams = rows[5].find_all('td')
 LiveScoresAPI.writeLine(teams)
+
+#get totals
+totals = rows[6].find_all('td')
+score = []
+for total in totals:
+    runs = total.text.split(' for')
+    print(runs[0])
